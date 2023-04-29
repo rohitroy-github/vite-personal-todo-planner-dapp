aa137ea06305bf1376d7458e8c13b57940621756
rohit <rhtry.tech@gmail.com>
2023-04-30 02:31:07 +0530

    add : backend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [editIndex, setEditIndex] = useState(-1);
   const [editText, setEditText] = useState("");
 
-  const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const CONTRACT_ADDRESS = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
 
   //  function : handleAnyChangeAnyInputFeild
   const handleInputChange = (event) => {
